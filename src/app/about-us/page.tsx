ed6a98c8c02315aa35d118ae9aec98f69de6bda2
Dgsmohan <mohan@studioq.co.in>
2025-04-24 16:15:19 +0530

    react/no-unescaped-entities - issue fix

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -15,16 +15,16 @@ const OurMenu = () => {
                 {/* Title End */}
                 <div className='w-full rounded-tr-[17px] rounded-br-[17px] overflow-hidden min-h-[57vh]'>
                     <p className='text-[17px] text-[#222A4A] leading-[22px] py-[17px]'>
-                        At Namma Restaurant, we’re more than just a place to eat — we’re a journey into the heart of India’s rich culinary tradition. From the first bite, our guests are transported into a world of bold, smoky flavors, lovingly crafted using ancient techniques that have been perfected for generations.
+                       {`At Namma Restaurant, we’re more than just a place to eat — we’re a journey into the heart of India’s rich culinary tradition. From the first bite, our guests are transported into a world of bold, smoky flavors, lovingly crafted using ancient techniques that have been perfected for generations.`}
                     </p>
                     <p className='text-[17px] text-[#222A4A] leading-[22px] py-[17px]'>
-                        Customers often say it’s not just the food that makes Namma special, but the experience. The sizzle of dishes prepared over hot stones, the aroma of food cooked in coal grills, and the unmistakable smokiness from our tandoori ovens all come together to create an atmosphere that feels both timeless and modern.
+                        {`Customers often say it’s not just the food that makes Namma special, but the experience. The sizzle of dishes prepared over hot stones, the aroma of food cooked in coal grills, and the unmistakable smokiness from our tandoori ovens all come together to create an atmosphere that feels both timeless and modern.`}
                     </p>
                     <p className='text-[17px] text-[#222A4A] leading-[22px] py-[17px]'>
-                        Our chefs are true artisans, recreating the flavors of India with dedication and passion. Whether it’s a delicate, spicy curry or perfectly grilled meats, each dish carries a piece of the story behind it. It’s not just about the food; it’s about the journey it takes you on — one where you can taste the heritage and history of India in every bite.
+                        {`Our chefs are true artisans, recreating the flavors of India with dedication and passion. Whether it’s a delicate, spicy curry or perfectly grilled meats, each dish carries a piece of the story behind it. It’s not just about the food; it’s about the journey it takes you on — one where you can taste the heritage and history of India in every bite.`}
                     </p>
                     <p className='text-[17px] text-[#222A4A] leading-[22px] py-[17px]'>
-                        But don't just take our word for it. Our customers rave about the experience, with many calling it “the best Indian food they've ever had,” and noting how they feel like they’ve stepped into an authentic, warm, and welcoming corner of India. Whether you're a lifelong fan of Indian cuisine or new to the flavors, Namma Restaurant promises an unforgettable, flavorful experience that will have you coming back for more.
+                        {`But don't just take our word for it. Our customers rave about the experience, with many calling it “the best Indian food they've ever had,” and noting how they feel like they’ve stepped into an authentic, warm, and welcoming corner of India. Whether you're a lifelong fan of Indian cuisine or new to the flavors, Namma Restaurant promises an unforgettable, flavorful experience that will have you coming back for more.`}
                     </p>
                 </div>
             </div>
